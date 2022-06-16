f5792be90681f0a8d5c5c02ddfec5c5ca49e621a
Serhii <beschasnuy96@gmail.com>
2022-06-16 22:01:21 +0300

    created banner

--- src/components/Header/Banner/Banner.js
+++ src/components/Header/Banner/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './banner.css';
 
 const Banner = () => {
     return (
@@ -6,9 +7,11 @@ const Banner = () => {
             <div className="question">
                 <h4>How can we help?</h4>
             </div>
-            <div className="input-question">
-                <input type='text' placeholder="Search" />
-            </div>
+            <input
+                type='text'
+                placeholder="Search"
+                className='search'
+            />
         </div>
     );
 };
